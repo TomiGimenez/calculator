306b842d92c41dcac424244e398974dda45258ac
Tomi Gimenez <105307974+TomiGimenez@users.noreply.github.com>
2023-08-01 19:38:14 -0300

    Add basic calculator functionality

--- main.js
+++ main.js
@@ -1,3 +1,75 @@
+const numberButtons = document.querySelectorAll('#number-button');
+const operatorButtons = document.querySelectorAll('#operator-button');
+const equalsButton = document.querySelector('#equals-button');
+const clearButton = document.querySelector('#clear-button');
+const deleteButton = document.querySelector('#delete-button');
+const pointButton = document.querySelector('#point-button');
+const screen = document.querySelector('#calculator-screen');
+
+let firstOperand = "";
+let secondOperand = "";
+let operator = null;
+let shouldResetScreen = false;
+
+
+numberButtons.forEach((button) =>
+    button.addEventListener('click', () => 
+    appendNumber(button.textContent))
+);
+
+operatorButtons.forEach((button) =>
+    button.addEventListener('click', setOperator)
+);
+
+equalsButton.addEventListener('click', getScore);
+clearButton.addEventListener('click', clearScreen);
+deleteButton.addEventListener('click', deleteNumber);
+pointButton.addEventListener('click', setPoint);
+
+function appendNumber(number) {
+    if (screen.textContent === "0" || shouldResetScreen === true) resetScreen();
+    screen.textContent += number;
+}
+
+function setOperator(operatorButton) {
+    if (operator === null) {
+        firstOperand = screen.textContent;
+        operator = operatorButton.target.textContent;
+    } else {
+        operator = operatorButton.target.textContent;
+        getScore();
+    }
+    shouldResetScreen = true;
+}
+
+function getScore() {
+    secondOperand = screen.textContent;
+    firstOperand = operate(operator, firstOperand, secondOperand);
+    secondOperand = "";
+    screen.textContent = firstOperand;
+}
+
+function clearScreen() {
+    screen.textContent = "0";
+    firstOperand = "";
+    secondOperand = "";
+    operator = null;
+}
+
+function resetScreen() {
+    {
+      screen.textContent = "";
+      shouldResetScreen = false;
+    }
+}
+
+function deleteNumber() {}
+
+function setPoint() {
+    if (screen.textContent.includes(".") || screen.textContent === "") return;
+    screen.textContent += ".";
+}
+
 // Funcion suma
 function add(a, b) {
     return a + b;
@@ -29,6 +101,8 @@ function factorial(n) {
 };
 
 function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
     switch(operator) {
         case '+':
             return add(a, b);
@@ -38,8 +112,10 @@ function operate(operator, a, b) {
             return multiply(a, b);
         case '/':
             return divide(a, b);
-        case '**':
+        case '^':
             return power(a, b);
+        case '!':
+            return factorial(a);
         default:
             return;
     }
